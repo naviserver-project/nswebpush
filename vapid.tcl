e30bc1f59f54a5def2d14f6417da4b775b726a12
Gustaf Neumann <neumann@wu-wien.ac.at>
2018-04-27 17:00:39 +0200

    adjust to updated interface minor cleanup ihttps://jwt.io/ signature test works now with the updated version of naviserver

--- vapid.tcl
+++ vapid.tcl
@@ -1,4 +1,10 @@
-set ::vapidCertPath "[ns_info home]/modules//usr/local/ns/modules/vapid"
+#
+# Test file for VAPID token generation
+# RFC 8292: Voluntary Application Server Identification (VAPID)
+#
+
+set ::vapidCertPath "[ns_info home]/modules/vapid"
+
 if {![file exists $::vapidCertPath/prime256v1_key.pem]} {
     #
     # create private key for vapid
@@ -21,22 +27,14 @@ if {![file exists $::vapidCertPath/public_key.txt]} {
     exec -ignorestderr openssl ec -in prime256v1_key.pem -outform DER | tail -c +8 | head -c 32 | base64 | tr -d '=' | tr '/+' '_-' > private_key.txt
 }
 
-# url save base64 encoding replaces "/" with "_", "+" with "-" and removes "=" and newlines
-proc base64url {data} {
-  return [string map {+ - / _ = {} \n {}} [ns_base64encode $data]]
-}
-
-proc decodebase64url {data} {
-  return [encoding convertto utf-8 [binary decode base64 [string map {- + _ /} $data]]
-}
 
 proc stripWhitespacesNewlines {str} {
   return [string map {" " {} \n {}} $str]
 }
 
 proc vapidToken {string} {
-    set hexSignature [::ns_crypto::md vapidsign -digest sha256 -pem $::vapidCertPath/prime256v1_key.pem $string]
-    return $string.[base64url [binary format H* $hexSignature]]
+    set signature [::ns_crypto::md vapidsign -digest sha256 -encoding base64url -pem $::vapidCertPath/prime256v1_key.pem $string]
+    return $string.$signature
 }
 
 
@@ -50,9 +48,9 @@ set claim [subst {
 
 
 # this is always the jwt header
-set JWTHeader [base64url {{"typ":"JWT","alg":"ES256"}}]
+set JWTHeader [ns_base64urlencode {{"typ":"JWT","alg":"ES256"}}]
 puts [stripWhitespacesNewlines $claim]
-set JWTbody [base64url [stripWhitespacesNewlines $claim]]
+set JWTbody [ns_base64urlencode [stripWhitespacesNewlines $claim]]
 
 # the JWT base string is the header and body separated with a "."
 set token [vapidToken $JWTHeader.$JWTbody]
@@ -62,25 +60,32 @@ set f [open $::vapidCertPath/public_key.txt]
 set pub_key [read $f]
 close $f
 
-set f [open $::vapidCertPath/public.pem]
-set pub_pem [read $f]
+set f [open $::vapidCertPath/private_key.txt]
+set priv_key [read $f]
+close $f
+
+
+set f [open $::vapidCertPath/prime256v1_key.pem]
+set pem [read $f]
 close $f
 
 ns_return 200 text/plain [subst {
     claim:
     $claim
 
-    VAPID token: <$token>
+    unsigned: $JWTHeader.$JWTbody
+
+    VAPID token: $token
+    VAPID token length: [string length $token]
 
     $::vapidCertPath/prime256v1_key.pem - [file size $::vapidCertPath/prime256v1_key.pem] bytes
     $::vapidCertPath/public_key.txt     - [file size $::vapidCertPath/public_key.txt] bytes
     $::vapidCertPath/private_key.txt    - [file size $::vapidCertPath/private_key.txt] bytes
 
-    public_key:
-    $pub_key
+    public_key : [string trim $pub_key]
+    private_key: [string trim $priv_key]
 
-    public.pem:
-    $pub_pem
+    prime256v1_key.pem\n$pem
 
     HOME: $::env(HOME)
 }]
