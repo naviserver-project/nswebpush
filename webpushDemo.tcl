04d5c969215771093ae029c451672c4a65a92bd8
Gustaf Neumann <neumann@wu-wien.ac.at>
2018-08-05 15:03:22 +0200

    load - for the time being - webpush.tcl from the same directory as the the calling script

--- webpushDemo.tcl
+++ webpushDemo.tcl
@@ -1,28 +1,60 @@
 package require json
 
-set subscription	[ns_queryget subscr]
+set subscription [ns_queryget subscr]
 set text [ns_queryget text]
 set pushstatus ""
 
+#
+# Load the Web Push API for NaviServer.
+#
+# For the timne being, we assume, we can load the API from the same
+# directory, from which this script is run. In general, it should be
+# loaded in advance via e.g. a module. This should be supported soon.
+#
+set script [ns_url2file [ns_conn url]]
+set scriptDir [file dirname $script]
+source $scriptDir/webpush.tcl
+ns_log notice "script $scriptDir/webpush.tcl was loaded"
+
 if {$subscription ne ""} {
-  # load webpush
-  source [ns_info home]/pages/pushnotificationsapi/webpush.tcl
-  # subscription will be in json
-  set subscrDict [::json::json2dict $subscription]
-  set keys [dict get $subscrDict keys]
-  # transform to correctly formatted tcl dict
-  set subscription [subst {endpoint [dict get $subscrDict endpoint] auth [dict get $keys auth] p256dh [dict get $keys p256dh]}]
-  # private key is here
-  set privPem "[ns_info home]/modules/vapid/prime256v1_key.pem"
-  # local path
-  set localPath "[ns_info home]/pages/pushnotificationsapi"
-  # send push notification
-  if {[webpush $subscription $text {sub mailto:georg@test.com} $privPem $localPath aesgcm 60] < 300} {
-    set pushstatus "Push notification sent successfully!"
-  } else {
-    set pushstatus "Some error occurred when sending push notification!"
-  }
+    #
+    # Subscription will be in JSON
+    #
+    set subscrDict [::json::json2dict $subscription]
+    set keys [dict get $subscrDict keys]
+
+    #
+    # transform to correctly formatted tcl dict
+    #
+    set subscription [subst {endpoint [dict get $subscrDict endpoint] auth [dict get $keys auth] p256dh [dict get $keys p256dh]}]
+
+    #
+    # private key is here
+    #
+    set privPem "[ns_info home]/modules/vapid/prime256v1_key.pem"
+
+    #
+    # Local path
+    #
+    set localPath $scriptDir
+
+    #
+    # send push notification
+    #
+    if {[webpush::send \
+	     -subscription $subscription \
+	     -data $text \
+	     -claim {sub mailto:georg@test.com} \
+	     -privateKeyPem $privPem \
+	     -localKeyPath $localPath \
+	     -mode aesgcm \
+	     -timeout 60] < 300} {
+	set pushstatus "Push notification sent successfully!"
+    } else {
+	set pushstatus "Some error occurred when sending push notification!"
+    }
 }
+
 
 ns_return 200 text/html [subst {
   <html>
